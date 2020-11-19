22579223714cb28b68d94c973d9a05918ffefcde
EasyWin6 <sunshineregi888@gmail.com>
2020-11-19 17:55:54 +0200

    All tasks is true))

--- t02/t02/bin/t02.dart
+++ t02/t02/bin/t02.dart
@@ -1,1 +1,11 @@
-void main() {}
+var list1 = [1, 2];
+
+void main() {
+  var result = 0;
+
+  for (var i = 0; i < list1.length; i++) {
+    result += (list1[i] > 0 ? list1[i] : 0);
+  }
+
+  print(result);
+}
